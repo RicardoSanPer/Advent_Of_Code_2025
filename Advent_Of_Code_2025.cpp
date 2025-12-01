cfe322ad148676b9e7bc26dc402a34e07f2349bd
RicardoSP <ricardosp@ciencias.unam.mx>
2025-12-01 10:53:58 -0600

    Comand line argument value moved to function

--- Advent_Of_Code_2025.cpp
+++ Advent_Of_Code_2025.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+int extractProblemNumber(char *argv[]);
+
 int main(int argc, char *argv[])
 {
     //A problem number has to be supplied
@@ -9,22 +11,37 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    int selected_problem = 0;
+    int selected_problem = extractProblemNumber(argv);
 
-    for (int i = 0; argv[1][i] != '\0'; i++)
+    if (selected_problem < 1)
     {
-        selected_problem *= 10;
-        int digit = argv[1][i] - '0';
-        
-        if (digit < 0 || digit > 9)
-        {
-            std::cerr << "Invalid number: " << argv[1] << std::endl;
-            return 0;
-        }
-
-        selected_problem += digit;
+        std::cerr << "Invalid problem number: " << argv[1] << std::endl;
+        return 0;
     }
     
     std::cout << selected_problem << std::endl;
     
+}
+
+/// @brief Extracts the numeric value of the first command line argument, assuming it is an positive integer
+/// @param argv Command line arguments
+/// @return Numeric value, -1 if it is not a positive integer
+int extractProblemNumber(char* argv[])
+{
+    int value = 0;
+
+    //Iterate through each digit until \0
+    for (int i = 0; argv[1][i] != '\0'; i++)
+    {
+        value *= 10;
+        int digit = argv[1][i] - '0';
+
+        if (digit < 0 || digit > 9)
+        {
+            return -1;
+        }
+
+        value += digit;
+    }
+    return value;
 }
