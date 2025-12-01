8250bf3918c5f2b711dc9e455c8c1e936e39d500
RicardoSP <ricardosp@ciencias.unam.mx>
2025-12-01 10:48:24 -0600

    Extraction of problem number from program arguments

--- Advent_Of_Code_2025.cpp
+++ Advent_Of_Code_2025.cpp
@@ -1,20 +1,30 @@
-// Advent_Of_Code_2025.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!\n";
+    //A problem number has to be supplied
+    if (argc < 2)
+    {
+        std::cout << "No problem supplied" << std::endl;
+        return 0;
+    }
+
+    int selected_problem = 0;
+
+    for (int i = 0; argv[1][i] != '\0'; i++)
+    {
+        selected_problem *= 10;
+        int digit = argv[1][i] - '0';
+        
+        if (digit < 0 || digit > 9)
+        {
+            std::cerr << "Invalid number: " << argv[1] << std::endl;
+            return 0;
+        }
+
+        selected_problem += digit;
+    }
+    
+    std::cout << selected_problem << std::endl;
+    
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
