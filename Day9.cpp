71ee99997dfa3e10fd28772fd4eb15308447e998
RicardoSP <ricardosp@ciencias.unam.mx>
2025-12-09 22:48:40 -0600

    Part 2

--- Day9.cpp
+++ Day9.cpp
@@ -1,5 +1,11 @@
 #include "Day9.h"
 
+/// Part 2:
+/// For every corner
+/// 1. Pic another corner
+/// 2. For every corner in the list, check if any cuts into the square:
+///		- If previous was outside rectangle and current is within (not including edges), rectangle is not valid
+
 void Day9::passInputLine(std::string input)
 {
 	std::string number;
@@ -35,6 +41,118 @@ void Day9::processPart1()
 	}
 }
 
+void Day9::processPart2()
+{
+	RedTile* currentTile;
+	RedTile* currentCorner;
+	for (int i = 0; i < tiles.size(); i++)
+	{
+		currentTile = tiles[i];
+		for (int j = i + 1; j < tiles.size(); j++)
+		{
+			currentCorner = tiles[j];
+			testCorners(currentTile, currentCorner);
+		}
+	}
+}
+
+
+void Day9::testCorners(RedTile* corner1, RedTile* corner2)
+{
+	uint64_t top = 0;
+	uint64_t bottom = 0;
+	uint64_t left = 0;
+	uint64_t right = 0;
+
+	//std::cout << "Testing: " << corner1->x << ", " << corner1->y << " : " << corner2->x << ", " << corner2->y << std::endl;
+
+	//Bounding box
+	if (corner1->x > corner2->x)
+	{
+		right = corner1->x;
+		left = corner2->x;
+	}
+	else
+	{
+		right = corner2->x;
+		left = corner1->x;
+	}
+
+	if (corner1->y > corner2->y)
+	{
+		top = corner1->y;
+		bottom = corner2->y;
+	}
+	else
+	{
+		top = corner2->y;
+		bottom = corner1->y;
+	}
+
+	RedTile* testTile;
+
+	uint64_t x = tiles[0]->x;
+	uint64_t y = tiles[0]->y;
+
+	uint64_t prevx = tiles[tiles.size() - 1]-> x;
+	uint64_t prevy = tiles[tiles.size() - 1]-> y;
+
+	uint64_t area = 0;
+	for (int i = 0; i < tiles.size(); i++)
+	{
+		testTile = tiles[i];
+		x = testTile->x;
+		y = testTile->y;
+
+		//std::cout << "\tTesting edge: " << prevx << ", " << prevy << " : " << x << ", " << y << " ";
+
+		if (prevx <= left && x <= left)
+		{
+			//std::cout << "Out (LEFT)" << std::endl;
+			prevx = x;
+			prevy = y;
+			continue;
+		}
+
+		if (prevx >= right && x >= right)
+		{
+			//std::cout << "Out (RIGHT)" << std::endl;
+			prevx = x;
+			prevy = y;
+			continue;
+		}
+
+		if (prevy <= bottom && y <= bottom)
+		{
+			//std::cout << "Out (BOTTOM)" << std::endl;
+			prevx = x;
+			prevy = y;
+			continue;
+		}
+
+		if (prevy >= top && y >= top)
+		{
+			//std::cout << "Out (TOP)" << std::endl;
+			prevx = x;
+			prevy = y;
+			continue;
+		}
+		
+		//std::cout << "FAILED" << std::endl;
+
+		return;
+
+	}
+
+	area = (1 + right - left) * (1 + top - bottom);
+	if (area > answer2)
+	{
+		answer2 = area;
+		//std::cout << "Largest: " << corner1->x << ", " << corner1->y << " and " << corner2->x << ", " << corner2->y << std::endl;
+	}
+}
+
+
 uint64_t Day9::getArea(RedTile* tile1, RedTile* tile2)
 {
 	uint64_t x = 0;
@@ -64,6 +182,8 @@ uint64_t Day9::getArea(RedTile* tile1, RedTile* tile2)
 void Day9::printAnswer()
 {
 	processPart1();
+	processPart2();
 
-	std::cout << answer1 << std::endl;
+	std::cout << "Answer to part 1: " <<  answer1 << std::endl;
+	std::cout << "Answer to part 2: " <<  answer2 << std::endl;
 }
--- Day9.h
+++ Day9.h
@@ -11,9 +11,13 @@ struct RedTile
 class Day9 : public Problem
 {
 	uint64_t answer1 = 0;
+	uint64_t answer2 = 0;
 	std::vector<RedTile*> tiles;
 
 	void processPart1();
+	void processPart2();
+	void testCorners(RedTile* corner1, RedTile* corner2);
+
 	uint64_t getArea(RedTile* tile1, RedTile* tile2);
 
 	void passInputLine(std::string input) override;
